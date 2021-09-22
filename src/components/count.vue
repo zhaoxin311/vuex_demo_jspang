26cd7fa4318d96e61ce0bae26a5c87fdbb53d02e
zhaoxin0311 <1204090769@qq.com>
2021-09-22 21:50:19 +0800

    Vuex的小demo

---
+++ src/components/count.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <h3>{{ $store.state.count }}</h3>
+    <p><button @click="$store.commit('add')">+</button></p>
+    <p><button @click="$store.commit('cut')">-</button></p>
+  </div>
+</template>
+<script>
+import store from "@/store/index";
+export default {
+  data() {
+    return {
+      message: "vuex_demo",
+    };
+  },
+  store,
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,25 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+//vuex是一个数据仓库 也是一个状态管理器
+//声明一个常量 state 相当于一个状态
+const state = {
+  count: 10,
+};
+//用于改变状态state里的值，里边可以写一些方法
+const mutations = {
+  add(state) {
+    state.count++;
+  },
+  cut(state) {
+    state.count--;
+  },
+};
+
+//用export default 封装代码，让外部可以引用。
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state,
+  mutations,
   actions: {},
   modules: {},
 });
